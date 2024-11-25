74f8b9a9c5fabbabdba583418cec0e51ab9b40b7
Mr. Robot <gomexxxx71@gmail.com>
2024-11-25 11:08:23 +0600

    chore: configure formkit

---
+++ formkit.config.ts
@@ -0,0 +1,6 @@
+import { defineFormKitConfig } from '@formkit/vue'
+export default defineFormKitConfig({
+  // rules: {},
+  // locales: {},
+  // etc.
+})
--- pages/new.vue
+++ pages/new.vue
@@ -1,5 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { FormKit } from '@formkit/vue'
+const name = ref('')
+const desc = ref('')
+</script>
 
 <template>
   <h3>Add new book</h3>
+  <FormKit
+    type="text"
+    label="FormKit Input"
+    help="edit me to get started"
+    v-model="name"
+  />
+
+  <p>{{ name }}</p>
 </template>
